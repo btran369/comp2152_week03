4d17f25f3e59e773ddeec65ac3fd875c8ea7d176
btran369 <VuAnhQuan.Tran@georgebrown.ca>
2025-01-24 15:56:11 -0500

    update lab03.py

--- lab03.py
+++ lab03.py
@@ -1,2 +1,16 @@
 # lab 3 coding questions
+# dice
 diceOptions = list(range(1,7))
+
+# weapons
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear bomb"] 
+
+print("Available Weapons: ", ", ".join(weapons))
+
+def getCombatStrength(prompt):
+    while True:
+        value = input(prompt)
+        if 1 <= value <= 6:
+            return value
+        else:
+            print("Invalid input, enter value from 1-6")
